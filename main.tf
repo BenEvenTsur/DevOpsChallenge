839004242e858a0e2723117b61c87635c3502724
BenEvenTsur <beneventsur@gmail.com>
2022-05-26 18:56:07 +0300

    Adding Terraform providers declaration

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.15.1"
+    }
+  }
+
+  required_version = ">= 1.2.1"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
