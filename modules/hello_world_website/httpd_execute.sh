2be71ff0070a474253d84119ce0c946af4ec72f9
BenEvenTsur <beneventsur@gmail.com>
2022-05-26 23:23:58 +0300

    The index.html dynamic file was enriched

--- modules/hello_world_website/httpd_execute.sh
+++ modules/hello_world_website/httpd_execute.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 
-echo "<html><body><p>Hello World</p></body></html>" > index.html
+cat <<EOF > index.html
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Hello World Website</title>
+        <style>
+center{
+	color: #7150c7;
+    font-size: 40px;
+    font-weight: 500;
+    font-family: sans-serif;
+    margin-top: 30px;
+}
+        </style>
+	</head>
+	<body>
+		<center>Hello World</center>
+	</body>
+</html>
+EOF
+
 nohup busybox httpd -f -p ${http_port} &
